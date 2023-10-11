d82275ab85693f4b1b6c4ceefc2c58b97bc2f218
sevtou <sevtou123@gmail.com>
2023-10-11 22:04:20 +0700

    release 0.1

--- app/src/main/java/annk/example/diceroller/MainActivity.kt
+++ app/src/main/java/annk/example/diceroller/MainActivity.kt
@@ -2,10 +2,25 @@ package annk.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bnt : Button = findViewById(R.id.button2)
+        var lab : TextView = findViewById(R.id.textView)
+        var rand : Random = Random();
+
+        bnt.setOnClickListener() {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            lab.setText((rand.nextInt(6)+1).toString());
+        }
     }
 }
