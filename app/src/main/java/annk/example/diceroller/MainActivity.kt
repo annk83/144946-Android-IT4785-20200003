e65c229d5f294f46c30384c721d78198ff9076d6
sevtou <sevtou123@gmail.com>
2023-10-11 22:11:22 +0700

    release 0.2

--- app/src/main/java/annk/example/diceroller/MainActivity.kt
+++ app/src/main/java/annk/example/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var bnt : Button = findViewById(R.id.button2)
-        var lab : TextView = findViewById(R.id.textView)
-        var rand : Random = Random();
+        //Get Views
+        val bnt : Button = findViewById(R.id.button2)
+        val lab : TextView = findViewById(R.id.textView)
+        val lab1 : TextView = findViewById(R.id.textView2)
+
+        //Random for the dice
+        val rand = Random();
 
         bnt.setOnClickListener() {
+            //Show toast from bottom of screen
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
+
+            //Set text to random
             lab.setText((rand.nextInt(6)+1).toString());
+            lab1.setText((rand.nextInt(6)+1).toString());
         }
     }
 }
